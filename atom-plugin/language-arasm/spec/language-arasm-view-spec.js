1d56934c780bb66297ecfcbc454a6f3fdcc1dde6
sesshomariu <sesshotv@t-online.de>
2017-07-22 00:04:01 +0200

    Moved plugin itself to a seperate repo: https://github.com/sesshomariu/arasm-plugin

--- atom-plugin/language-arasm/spec/language-arasm-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageArasmView from '../lib/language-arasm-view';
-
-describe('LanguageArasmView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
